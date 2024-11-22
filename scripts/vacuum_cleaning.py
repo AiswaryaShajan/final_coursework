addeeb316f39bd133687e96a5d4b2ea9614e8ad9
Aiswarya Shajan <AS4213@live.mdx.ac.uk>
2024-11-21 23:23:33 -0800

    work on moving horizontally:
    implement flags to check for the conditions and move to next.
    divide move horizontal into reach right end of window, turn 90, move straight , turn 90, and move back

--- scripts/vacuum_cleaning.py
+++ scripts/vacuum_cleaning.py
@@ -5,14 +5,22 @@ from turtlesim.msg import Pose
 twist = Twist()
 pose = Pose()
 print('Hey there! In order to clean your room, you have to tell me the coordinates of the four corners of your room, when prompted. See the example below: \n top-left corner : 3 <add a spac>e 4 if the corner is at (3,4)')
-x1, y1 = map(float, input("Bottom left corner : ").split())
-x2, y2 = map(float, input("Bottom right corner: ").split())
-x3, y3 = map(float, input("Top left corner : ").split())
-x4, y4 = map(float, input("Top right corner: ").split())
-get_time = None
+
+x1= 0
+y1=0
+x2= 11
+y2=0
+x3= 0
+y3=11
+x4=11
+y4=11
+reach_x2 = False
+turned_90_once = False
+moved_lil_straight= False
+turned_90_twice = False
+reached_x1 = False
 
 def callback(msg):
-    rospy.loginfo(f"The Turtle is at x= {msg.x}, y= {msg.y}")
     global pose
     pose=msg
 
@@ -22,41 +30,59 @@ def vacuum_cleaning():
     pub = rospy.Publisher('/turtle2/cmd_vel', Twist, queue_size=10)
     rospy.Subscriber('/turtle2/pose', Pose, callback)
     rate = rospy.Rate(10)
-    time = (x2-x1)/2
     
     def move_horizontal():
-        if pose.x < x2:
-            print('move right')
-            twist.linear.x=2
-            twist.angular.z=0
-            pub.publish(twist)
-            rospy.sleep(0.5)
-        else:
-            print('turning now')
-            twist.linear.x= 0
-            twist.angular.z=1.5
-            pub.publish(twist)
-            rospy.sleep(0.5)
-            print('going straight for a while')
-            twist.linear.x= 2
-            twist.angular.z=0
-            pub.publish(twist)
-            rospy.sleep(0.5)
-            print('turning again')
-            twist.linear.x= 0
-            twist.angular.z=1.5
-            pub.publish(twist)
-            rospy.sleep(0.5)
-            if pose.x >= x1:
-                print('move left')
-                twist.linear.x= 2
+        global reach_x2, turned_90_once, moved_lil_straight, turned_90_twice, reached_x1
+        if pose.x <x2:
+            if reach_x2 == False:
+                print('move right')
+                twist.linear.x=2
                 twist.angular.z=0
-                
-            else:
-                print('reached and turning back')
-                twist.linear.x=0
-                twist.angular.z = 3
+                pub.publish(twist)
+                rospy.sleep(0.5)
+        else:
+            reach_x2=True
+            twist.linear.x=0
             pub.publish(twist)
+            if reach_x2 == True and turned_90_once == False:
+                print('stopped to turn')
+                twist.linear.x= 0
+                twist.angular.z=1.5
+                pub.publish(twist)
+                turned_90_once = True
+                rospy.sleep(0.5)
+            elif reach_x2 and turned_90_once and not moved_lil_straight:
+                print('to move a lil straight')
+                twist.angular.z=0
+                twist.linear.x=2
+                pub.publish(twist)
+                rospy.sleep(0.5)
+                moved_lil_straight = True
+            elif reach_x2 and turned_90_once and moved_lil_straight and turned_90_twice == False:
+                print("turning 90 again")
+                twist.linear.x= 0
+                twist.angular.z=4.7
+                pub.publish(twist)
+                turned_90_twice = True
+                rospy.sleep(0.5)
+
+            elif moved_lil_straight and turned_90_twice and not reached_x1:
+                if pose.x >x1:
+                    twist.angular.z=0
+                    print('lets move back')
+                    twist.linear.x = 2
+                else:
+                    print('reached x1')
+                    twist.linear.x= 0
+                    reached_x1 = True
+                pub.publish(twist)
+
+
+            
+                
+
+                
+        
 
 
     while not rospy.is_shutdown():
